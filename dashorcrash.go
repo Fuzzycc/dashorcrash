231d108870938dedc670b16995c94096ea8d4570
Fuzzycc-local1 <70171293+Fuzzycc@users.noreply.github.com>
2025-05-23 18:28:23 +0300

    Changes to be committed: 	new file:   dashorcrash.go 	new file:   dashorcrash_test.go 	new file:   go.mod

---
+++ dashorcrash.go
@@ -0,0 +1,115 @@
+package dashorcrash
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// ------------
+// Dash section
+// ------------
+
+// Dash is equivalent to [DashOut(err, "")]
+//
+// Useful if you want to ignore certain errors, but want to log them to standard out with no added prefix.
+// Use [DashBool] instead if you want no error message logging
+func Dash(err error) (erred bool) {
+	return DashOut(err, "")
+}
+
+// Dash ignores an error, but returns whether it was non-nil.
+//
+// Useful if you want to ignore certain errors, but want to log if any were encountered.
+func DashBool(err error) (erred bool) {
+	if err != nil {
+		return true
+	}
+	return false
+}
+
+// DashBlind ignores any error with no return whatsoever.
+//
+// Run, run and never look back! Aaaaah!
+func DashBlind(err error) {
+	// What did you expect?
+	return
+}
+
+// DashStr ignores an error, but returns its string format appended to prefix. Empty if err is nil.
+//
+// Useful if you want to ignore certain errors, but want to log them.
+func DashStr(err error, prefix string) (errString string) {
+	if err != nil {
+		return fmt.Sprintf("%s: %s", prefix, err)
+	}
+	return ""
+}
+
+// DashOut ignores an error, but returns whether it was non-nil and prints it to standard out appended to prefix.
+// Only when erred is true does it print.
+//
+// Useful if you want to ignore certain errors, but want to log them to Stdout.
+func DashOut(err error, prefix string) (erred bool) {
+	if err != nil {
+		fmt.Printf("%s: %s\n", prefix, err)
+		return true
+	}
+	return false
+}
+
+// DashStderr ignores an error, but returns whether it was non-nil and prints it to standard error appended to prefix.
+// Only when erred is true does it print.
+//
+// Useful if you want to ignore certain errors, but want to log them to Stderr.
+func DashErr(err error, prefix string) (erred bool) {
+	if err != nil {
+		_, errWrite := fmt.Fprintf(os.Stderr, "%s: %s\n", prefix, err)
+
+		tolerance := 100 // Doubt FprintF will fail to write -tolerance- times, but...
+		for errWrite != nil {
+			if tolerance <= 0 {
+				break
+			}
+			_, errWrite = fmt.Fprintf(os.Stderr, "Dashorcrash.DashStderr(): %s\n", err)
+			tolerance--
+		} // ... is this necessary?
+		return true
+	}
+	return false
+}
+
+// ------------
+// Crash Section
+// ------------
+
+// Crash is equivalent to [CrashOut(err, "")]
+//
+// Useful if you want to terminate on certain errors while logging them to standard out with no added prefixes.
+func Crash(err error) {
+	CrashOut(err, "")
+}
+
+// CrashOut prints err to standard output if non-nil followed by os.Exit(1), else nothing happens.
+//
+// Useful if you want to terminate on certain errors while logging them to standard out.
+func CrashOut(err error, prefix string) {
+	if err != nil {
+		if prefix != "" {
+			log.Fatalf("%s: %s\n", prefix, err)
+		}
+		log.Fatalf("%s\n", err)
+	}
+	return
+}
+
+func CrashErr(err error, prefix string) {
+	if err != nil {
+		if prefix != "" {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", prefix, err)
+		} else {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+		}
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Fuzzycc/dashorcrash
+
+go 1.24.1
